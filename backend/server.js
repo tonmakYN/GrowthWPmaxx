3b34332cfb73e2e06a10e460b281c4eee0753708
YN Barber <tonmak2553@gmail.com>
2025-09-04 21:39:15 +0700

    fix server js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
